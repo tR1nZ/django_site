11665deb29fb842aeaf5726e9db953458150e2ad
tR1nZ <lesnikovvova111@yandex.ru>
2022-06-23 21:22:58 +0300

    lesson8

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -13,9 +13,10 @@ class ShopUserAdminEditForm(ShopUserEditForm):
         
         
 class ProductCategoryEditForm(forms.ModelForm):
+    discount = forms.IntegerField(label='скидка', required=False, min_value=0, max_value=90, initial=0)
     class Meta:
         model = ProductCategory
-        fields = '__all__'
+        exclude = ()
     
     def __init__(self, *args, **kwargs):
         super(ProductCategoryEditForm, self).__init__(*args, **kwargs)
@@ -34,3 +35,22 @@ class ProductEditForm(forms.ModelForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             field.help_text = ''
+
+class ProductCategoryUpdateView(UpdateView):
+    model = ProductCategory
+    template_name = 'adminapp/category_update.html'
+    success_url = reverse_lazy('admin:categories')
+    form_class = ProductCategoryEditForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'категории/редактирование'
+        return context
+
+    def form_valid(self, form):
+        if 'discount' in form.cleaned_data:
+            discount = form.cleaned_data['discount']
+            if discount:
+                self.object.product_set.update(price=F('price') * (1 - discount / 100))
+                db_profile_by_type(self.__class__, 'UPDATE',connection.queries)
+        return super().form_valid(form)
--- adminapp/views.py
+++ adminapp/views.py
@@ -9,7 +9,9 @@ from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+from django.db import connection
 from django.views.generic.detail import DetailView
 
 
@@ -194,3 +196,17 @@ def product_delete(request, pk):
     }
     
     return render(request, 'adminapp/product_delete.html', content)
+
+def db_profile_by_type(prefix, type, queries):
+    update_queries = list(filter(lambda x: type in x['sql'], queries))
+    print(f'db_profile {type} for {prefix}:')
+    [print(query['sql']) for query in update_queries]
+
+@receiver(pre_save, sender=ProductCategory)
+def product_is_active_update_productcategory_save(sender, instance, **kwargs):
+    if instance.pk:
+        if instance.is_active:
+            instance.product_set.update(is_active=True)
+        else:
+            instance.product_set.update(is_active=False)
+        db_profile_by_type(sender, 'UPDATE', connection.queries)
